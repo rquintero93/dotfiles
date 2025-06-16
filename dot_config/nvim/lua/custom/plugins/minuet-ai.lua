57bca4763bdfd2eb8cd4ca051b46b1a9a3d836f7
Ricardo Quintero <rquinterog93@gmail.com>
2025-06-16 13:49:34 +0200

    Update .config/nvim/lua/custom/plugins/minuet-ai.lua

--- dot_config/nvim/lua/custom/plugins/minuet-ai.lua
+++ dot_config/nvim/lua/custom/plugins/minuet-ai.lua
@@ -10,6 +10,16 @@ return {
             api_key = 'CODESTRAL_API_KEY',
             optional = { max_tokens = 256, stop = { '\n\n' } },
           },
+          openai_fim_compatible = {
+            api_key = 'TERM',
+            name = 'Ollama',
+            end_point = 'http://localhost:11434/v1/completions',
+            model = 'qwen2.5-coder:7b',
+            optional = {
+              max_tokens = 56,
+              top_p = 0.9,
+            },
+          },
         },
       }
     end,
