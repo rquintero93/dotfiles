2ebf0a792c619a7dadc9bb48ce5886a392ca00a9
Ricardo Quintero <rquinterog93@gmail.com>
2025-07-16 22:38:00 +0200

    Update .config/nix/home.nix

--- dot_config/nix/home.nix
+++ dot_config/nix/home.nix
@@ -28,8 +28,95 @@
   
   programs.aerospace = {
     enable = true;
-    # userSettings = {
-    # };
+    userSettings = {
+      start-at-login = true;
+      enable-normalization-flatten-containers = true;
+      enable-normalization-opposite-orientation-for-nested-containers = true;
+      accordion-padding = 20;
+
+      default-root-container-layout = "accordion";
+      
+      default-root-container-orientation = "auto";
+      on-focused-monitor-changed = ["move-mouse monitor-lazy-center"];
+
+      key-mapping.preset = "qwerty";
+
+      gaps = {
+        inner.horizontal = 20;
+        inner.vertical = 20;
+        outer.left = 20;
+        outer.bottom = 20;
+        outer.top = 20;
+        outer.right = 20;
+      };
+      mode.main.binding = {
+        alt-slash = "layout tiles horizontal vertical";
+        alt-comma = "layout accordion horizontal vertical";
+
+        alt-h = "focus left";
+        alt-j = "focus down";
+        alt-k = "focus up";
+        alt-l = "focus right";
+
+        alt-shift-h = "move left";
+        alt-shift-j = "move down";
+        alt-shift-k = "move up";
+        alt-shift-l = "move right";
+
+        alt-shift-minus = "resize smart -50";
+        alt-shift-equal = "resize smart +50";
+
+        alt-1 = "workspace 1";
+        alt-2 = "workspace 2";
+        alt-3 = "workspace 3";
+        alt-4 = "workspace 4";
+
+        alt-shift-1 = "move-node-to-workspace 1";
+        alt-shift-2 = "move-node-to-workspace 2";
+        alt-shift-3 = "move-node-to-workspace 3";
+        alt-shift-4 = "move-node-to-workspace 4";
+        alt-shift-f = "fullscreen";
+        alt-tab = "workspace-back-and-forth";
+        alt-ctrl-tab = "move-workspace-to-monitor --wrap-around next";
+        alt-ctrl-shift-tab = "move-workspace-to-monitor --wrap-around prev";
+
+        alt-q = "close";
+
+        alt-shift-semicolon = "mode service";
+      };
+      mode.service.binding = {
+        esc = ["reload-config" "mode main"];
+        r = ["flatten-workspace-tree" "mode main"];
+        f = [
+            "layout floating tiling"
+            "mode main"
+        ];
+        backspace = ["close-all-windows-but-current" "mode main"];
+
+
+        alt-shift-h = ["join-with left" "mode main"];
+        alt-shift-j = ["join-with down" "mode main"];
+        alt-shift-k = ["join-with up" "mode main"];
+        alt-shift-l = ["join-with right" "mode main"];
+
+        down = "volume down";
+        up = "volume up";
+        shift-down = ["volume set 0" "mode main"];
+
+        };
+
+      workspace-to-monitor-force-assignment = {
+          "1" = "built-in";
+          "2" = "main";
+          "3" = "^BK550Y$";
+          "4" = "Z1";
+            };
+      
+      # on-window-detected = {
+      #     if.app-id = "com.mitchellh.ghostty";
+      #     run = "move-node-to-workspace 2";
+      #   };
+      };
   };
   
   # programs.git = {
