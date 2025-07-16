c948b7c36831efdcb2e57e6b2ac3797d580029cd
Ricardo Quintero <rquinterog93@gmail.com>
2025-07-16 20:44:58 +0200

    Update .config/nix/flake.lock Update .config/nix/flake.nix

--- dot_config/nix/flake.nix
+++ dot_config/nix/flake.nix
@@ -6,14 +6,14 @@
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, flake-utils }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs,  flake-utils }:
   let
     configuration = { pkgs, ... }: {
       # Necessary for using flakes on this system.
@@ -57,11 +57,11 @@
       };
 
       # Enable Home Manager
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users.ricardoquintero = import ./home.nix;
-      };
+      # home-manager = {
+      #   useGlobalPkgs = true;
+      #   useUserPackages = true;
+      #   users.ricardoquintero = import ./home.nix;
+      # };
 
     };
   in
@@ -86,7 +86,7 @@
     darwinConfigurations."MacBook-Pro" = nix-darwin.lib.darwinSystem {
       modules = [
         configuration
-        home-manager.darwinModules.home-manager
+        # home-manager.darwinModules.home-manager
       ];
     };
 
