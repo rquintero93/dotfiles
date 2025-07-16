a64a06e865025bfa694cb95f563b09665e7b1dd9
Ricardo Quintero <rquinterog93@gmail.com>
2025-07-16 18:57:11 +0200

    Update .config/nix/flake.nix Update .zshrc

--- dot_config/nix/flake.nix
+++ dot_config/nix/flake.nix
@@ -16,67 +16,6 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
-        commonPackages = pkgs: with pkgs;
-        [ 
-          age
-          atuin
-          awscli
-          bat
-          btop
-          chezmoi
-          cmake
-          cmatrix
-          curl
-          delta
-          eza
-          fastfetch
-          fd
-          ffmpeg
-          fzf
-          gemini-cli
-          gh
-          git
-          git-extras
-          glow
-          go
-          google-cloud-sdk
-          imagemagick
-          lazydocker
-          lazygit
-          llm
-          lua
-          luarocks
-          mongodb-cli
-          mongosh
-          navi
-          neovim
-          nodejs
-          ollama
-          openssl_3
-          python3
-          python313Packages.ipython
-          python313Packages.jupytext
-          python313Packages.pip
-          rbenv
-          ripgrep
-          sesh
-          sqlite-utils
-          sqlfluff
-          starship
-          tectonic
-          tmux
-          tree-sitter
-          uv
-          vim
-          visidata
-          wget
-          yazi
-          zip
-          zoxide
-          zsh
-
-        ];
-
     configuration = { pkgs, ... }: {
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -111,7 +50,7 @@
 
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages = commonPackages pkgs;
+      environment.systemPackages = import ./packages.nix pkgs; 
 
       # Create /etc/zshrc that loads the nix-darwin environment.
       programs.zsh = {
@@ -135,6 +74,17 @@
         configuration
         home-manager.darwinModules.home-manager
       ];
+    };
+    devShells.aarch64-darwin.default = let
+    pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+    in pkgs.mkShell {
+      name = "global-devshell";
+      packages = import ./packages.nix pkgs; 
+      shellHook = ''
+        export STARSHIP_CONFIG=/dev/null
+        echo "Entering global devshell (isolated)"
+        exec zsh
+      '';
     };
   };
 }
