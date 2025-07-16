f4192810c5d9d21484953391703cdd9e6fcaf6e5
Ricardo Quintero <rquinterog93@gmail.com>
2025-07-16 18:57:06 +0200

    Add .config/nix/packages.nix

---
+++ dot_config/nix/packages.nix
@@ -0,0 +1,62 @@
+{ pkgs, ... }:
+
+with pkgs; [
+          age
+          atuin
+          awscli
+          bat
+          btop
+          chezmoi
+          cmake
+          cmatrix
+          curl
+          delta
+          eza
+          fastfetch
+          fd
+          ffmpeg
+          fzf
+          gemini-cli
+          gh
+          git
+          git-extras
+          glow
+          go
+          google-cloud-sdk
+          imagemagick
+          lazydocker
+          lazygit
+          llm
+          lua
+          luarocks
+          mongodb-cli
+          mongosh
+          navi
+          neovim
+          nodejs
+          ollama
+          openssl_3
+          python3
+          python313Packages.ipython
+          python313Packages.jupytext
+          python313Packages.pip
+          rbenv
+          ripgrep
+          sesh
+          sqlite-utils
+          sqlfluff
+          starship
+          tectonic
+          tmux
+          tree-sitter
+          uv
+          vim
+          visidata
+          wget
+          yazi
+          zip
+          zoxide
+          zsh
+
+        ]
+
